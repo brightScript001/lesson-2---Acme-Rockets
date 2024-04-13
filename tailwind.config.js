3b5c5f6f374d1f95b3a068b6e7b7f44b11803548
Success Bright <bs3webdevelop@gmail.com>
2024-04-13 15:25:10 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./build/*.html", "./build/js/*.js"],
   theme: {
     extend: {
+      colors: {
+        papayawhip: {
+          light: "#fef4e4",
+          DEFAULT: "#ffefd5",
+          dark: "#fee5bc",
+        },
+      },
       screens: {
         widescreen: { raw: "(min-aspect-ratio: 3/2)" },
         tallscreen: { raw: "(max-aspect-ratio: 13/20)" },
